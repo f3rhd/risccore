ec88fc4820d62f19f08796cb3a94144cce73bfb6
anqiranagizsiz <farhadsafarov1980@gmail.com>
2025-07-18 14:36:13 +0400

    main function is better now

--- assembly/main.cpp
+++ assembly/main.cpp
@@ -1,21 +1,18 @@
 #include "tokenizer/tokenizer.hpp"
 #include "parser/parser.hpp"
 #include "common/line.hpp"
+#include "code_gen/ast_analyser.hpp"
 #include <iostream>
 int main(int argc, char** argv){
     FILE *source_file = fopen("test.sm", "r");
-
     if(source_file == 0){
         printf("zort");
         return 1;
     }
+    Parser parser;
     std::vector<Line> lines = utils::get_lines(source_file);
-
-    for(Line& line : lines){
-
-        for(Token& token : line.tokens){
-
-            std::cout << token.word << std::endl;
-        }
-    }
+    std::vector<AST_Node*> heads = parser.parse_lines(lines);
+    if(ast_analyser::analyse_ast_lines(heads) == 1)
+        return 1;
+    parser.resolve_identifiers(heads);
 }
