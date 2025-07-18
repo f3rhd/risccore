88d9629042c24eb2d8ee7e43ff5d10028b1cfbd4
anqiranagizsiz <farhadsafarov1980@gmail.com>
2025-07-18 14:36:52 +0400

    ast analyser

---
+++ assembly/code_gen/ast_analyser.hpp
@@ -0,0 +1,6 @@
+#include "../common/utils.hpp"
+namespace ast_analyser{
+
+    //@Incomplete : Analyser should throw error when there are no colons. It does not do that for now.
+    int analyse_ast_lines(std::vector<AST_Node*> heads);
+}
